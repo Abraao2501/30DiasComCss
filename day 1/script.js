bef6b98f78953778970b4d756f1530ce22b28fed
Abraão Oliveira <abraaooliveira.eeq@gmail.com>
2021-01-15 17:44:47 -0300

    Initial

--- day 1/script.js
+++
@@ -1,5 +0,0 @@
-const btn = document.querySelector('button')
-btn.addEventListener('click' , () => {
-    var side1 = document.querySelector('.blue')
-    side1.classList.toggle('on')
-}) 
---
+++ day one/script.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('button')
+btn.addEventListener('click' , () => {
+    var side1 = document.querySelector('.blue')
+    side1.classList.toggle('on')
+}) 
